d22bd9253e94f2f073b52bf2298f4abd3fbfbc3a
DrRockyMC <65335475+rocky-co@users.noreply.github.com>
2023-01-11 08:37:07 +0800

    Create PluginBase.java

---
+++ src/main/java/dev/drrockymc/base/PluginBase.java
@@ -0,0 +1,20 @@
+package dev.drrockymc.base;
+
+import lombok.Getter;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class PluginBase extends JavaPlugin {
+    @Getter
+    public static PluginBase INSTANCE;
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+        System.out.println("[&ePluginName&f] Plugin Loaded! Version: 0.0.1");
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
